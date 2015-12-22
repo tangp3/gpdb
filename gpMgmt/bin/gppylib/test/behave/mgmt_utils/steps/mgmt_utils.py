48aeba4048b757fdfca77a19f143aecd8b685a95
Pengcheng Tang <petang@pivotal.io>
2015-12-21 23:21:40 -0800

    update the behave test

--- gpMgmt/bin/gppylib/test/behave/mgmt_utils/steps/mgmt_utils.py
+++ gpMgmt/bin/gppylib/test/behave/mgmt_utils/steps/mgmt_utils.py
@@ -961,9 +961,9 @@ def impl(context, tname, dbname, nrows):
 def impl(context, src_tname, src_dbname, dest_tname, dest_dbname, options):
     match_table_select(context, src_tname, src_dbname, dest_tname, dest_dbname, options)
 
-@then('verify that table "{src_tname}" in database "{src_dbname}" of source system has same data with table "{dest_tname}" in database "{dest_dbname}" of destination system with order by "{orderby}" with options "{options}"')
-def impl(context, src_tname, src_dbname, dest_table, dest_dbname, orderby, options):
-    match_table_select(context, src_tname, src_dbname, dest_tname, dest_dbname, orderby, options)
+@then('verify that table "{src_tname}" in database "{src_dbname}" of source system has same data with table "{dest_tname}" in database "{dest_dbname}" of destination system with order by "{orderby}"')
+def impl(context, src_tname, src_dbname, dest_table, dest_dbname, orderby):
+    match_table_select(context, src_tname, src_dbname, dest_tname, dest_dbname, orderby)
 
 @then('verify that partitioned tables "{table_list}" in "{dbname}" have {num_parts} partitions')
 @then('verify that partitioned tables "{table_list}" in "{dbname}" have {num_parts} partitions in partition level "{partitionlevel}"')
