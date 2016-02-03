41a044f163f05c3f2068ecd63fb05fe995e79ffa
Pengcheng Tang <petang@pivotal.io>
2016-02-03 14:54:01 -0800

    update the select.sql

---
+++ gpMgmt/bin/gppylib/test/behave/mgmt_utils/steps/data/special_chars/select_from_special_table.sql
@@ -0,0 +1,7 @@
+SET SEARCH_PATH=" S`~@#$%^&*()-+[{]}|\;: \'""/?><1 ";
+
+SELECT * FROM " heap_T`~@#$%^&*()-+[{]}|\;: \'""/?><1 " order by 1;
+
+SELECT * FROM " ao_T`~@#$%^&*()-+[{]}|\;: \'""/?><1 " order by 1;
+
+SELECT * FROM " co_T`~@#$%^&*()-+[{]}|\;: \'""/?><1 " order by 1;
--- gpMgmt/bin/gppylib/test/behave/mgmt_utils/steps/data/special_chars/special_schema_data.sql
+++
@@ -1,7 +0,0 @@
-SET SEARCH_PATH=" S`~@#$%^&*()-+[{]}|\;: \'""/?><1 ";
-
-SELECT * FROM heap order by 1;
-
-SELECT * FROM ao order by 1;
-
-SELECT * FROM co order by 1;
